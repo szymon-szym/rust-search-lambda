316c046069f4902b30fc76f28e154528c76be18f
szymon-szym <szymon.mich@gmail.com>
2023-12-19 20:23:42 +0100

    Initial commit

---
+++ test/rust-search.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RustSearch from '../lib/rust-search-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rust-search-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RustSearch.RustSearchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
